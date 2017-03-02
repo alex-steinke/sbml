574d5eface2bdc6c5bf1c5fa5f48899b10d749c4
Alexander Anatoljewitsch Steinke <a.steinke2@ncl.ac.uk>
2017-03-02 17:47:18 +0000

    SBML classes: Unit, UnitDefinition, Compartment, Species, Parameter str representaion of a class corresponds to one line of SBML short

--- SBMLsite/tests/test_sbml_classes.py
+++ SBMLsite/tests/test_sbml_classes.py
@@ -1,58 +1,67 @@
 from nose.tools import assert_equals
-from SBMLshort.sbml import Unit, UnitDefinition, Compartment
+from SBMLshort.sbml import *
 import json
 
 
-class TestUnit:
+class TestSbml:
     def setup(self):
         self.my_unit = Unit("kg", -1, 0, 1)
+        self.my_def = UnitDefinition("id")
+        self.my_comp = Compartment("id", 'name', 1)
+        self.my_comp2 = Compartment("id2")
+        self.my_spe = Species("id", "cell", 0)
+        self.my_spe2 = Species("id", "cell", 0, True, True, True, "test")
+        self.my_param = Parameter("k1", 10)
+        self.my_param2 = Parameter("k2", 10, "test", True)
 
     def teardown(self):
         pass
 
+
+class TestUnit(TestSbml):
     def test_init(self):
         assert_equals(self.my_unit.kind, "kg")
-        assert_equals(self.my_unit.exponent, -1)
-        assert_equals(self.my_unit.scale, 0)
-        assert_equals(self.my_unit.multiplier, 1)
+        assert_equals(self.my_unit.e, -1)
+        assert_equals(self.my_unit.s, 0)
+        assert_equals(self.my_unit.m, 1)
 
     def test_str(self):
-        assert_equals(str(self.my_unit), "kg: (10^(0+1))^-1")
+        assert_equals(str(self.my_unit), "kg:m=1 e=-1 s=0")
 
 
-class TestUnitDefinition:
-    def setup(self):
-        self.my_def = UnitDefinition("id", "name")
-
-    def teardown(self):
-        pass
-
+class TestUnitDefinition(TestSbml):
     def test_init(self):
         assert_equals(self.my_def.id, "id")
-        assert_equals(self.my_def.name, "name")
 
     def test_dict(self):
         assert_equals({c[0]: c[1] for c in self.my_def.dict()},
-                      {"id": "id", "name": "name"})
+                      {"id": "id"})
 
-    def test_to_json(self):
-        my_unit = Unit("kg", -1, 0, 1)
-        self.my_def.add_unit(my_unit)
-        assert_equals(self.my_def.to_json(),
-                      json.dumps({"listOfUnits": [{"kind": "kg",
-                                                   "exponent": -1,
-                                                   "multiplier": 1}],
-                                  "id": "id", "name": "name"}))
+    def test_str(self):
+        self.my_def.add_unit(self.my_unit)
+        self.my_def.add_unit(self.my_unit)
+        assert_equals(str(self.my_def),
+                      " id=kg:m=1 e=-1 s=0; kg:m=1 e=-1 s=0")
 
 
-class TestCompartment:
-    def setup(self):
-        self.my_comp = Compartment("id", 1, 'name')
-
-    def teardown(self):
-        pass
-
+class TestCompartment(TestSbml):
     def test_init(self):
-        assert_equals(self.my_comp.to_json(), json.dumps({"constant": 1,
-                                                          "id": "id",
-                                                          "name": "name"}))
+        assert_equals(self.my_comp.to_json(), json.dumps({"id": "id",
+                                                          "name": "name",
+                                                          "size": 1}))
+
+    def test_str(self):
+        assert_equals(str(self.my_comp), ' id=1 "name"')
+        assert_equals(str(self.my_comp2), ' id2')
+
+
+class TestSpecies(TestSbml):
+    def test_str(self):
+        assert_equals(str(self.my_spe), ' cell:id=0 ')
+        assert_equals(str(self.my_spe2), ' cell:[id]=0 sbc "test"')
+
+
+class TestParameter(TestSbml):
+    def test_str(self):
+        assert_equals(str(self.my_param), ' k1=10')
+        assert_equals(str(self.my_param2), ' k2=10v "test"')
