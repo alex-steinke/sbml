f724fbea1ac0ef8740449aa10165432788f45ef1
Alexander Anatoljewitsch Steinke <a.steinke2@ncl.ac.uk>
2017-03-03 14:56:41 +0000

    SBML classes completed

--- SBMLshort/sbml.py
+++ SBMLshort/sbml.py
@@ -4,8 +4,8 @@ import json
 class SbmlObject(object):
     def dict(self):
         for attr in self.__dict__:
-            if self.__dict__[attr]:
-                yield (attr, self.__dict__[attr])
+            if self.__dict__[attr] is not None and self.__dict__[attr] != []:
+                yield attr, self.__dict__[attr]
 
     def to_json(self):
         return json.dumps(self,
@@ -16,32 +16,39 @@ class Model:
     def __init__(self, name, substance_units, time_units, volume_units,
                  area_units, length_units, extent_units, conversion_factor):
         self.name = name
-        self.substanceUnits = substance_units
-        self.timeUnits = time_units
-        self.volumeUnits = volume_units
-        self.areaUnits = area_units
-        self.lengthUnits = length_units
-        self.extentUnits = extent_units
-        self.conversionFactor = conversion_factor
+        self.s = substance_units
+        self.t = time_units
+        self.v = volume_units
+        self.a = area_units
+        self.l = length_units
+        self.e = extent_units
+        self.c = conversion_factor
 
 
 class Unit(SbmlObject):
-    def __init__(self, kind, exponent, scale, multiplier):
+    def __init__(self, kind=None, exponent=None, scale=None, multiplier=None):
         self.kind = kind
-        self.exponent = exponent
-        self.scale = scale
-        self.multiplier = multiplier
+        self.e = exponent
+        self.s = scale
+        self.m = multiplier
 
     def __str__(self):
-        return "%s: (10^(%s+%s))^%s" % (self.kind, self.scale,
-                                        self.multiplier, self.exponent)
+        unit_dict = dict(self.dict())
+        unit_info = []
+        for attr, value in self.dict():
+            if attr != 'kind':
+                unit_info.append(str(attr) + '=' + str(value))
+        return '%s:%s' % (unit_dict['kind'], ' '.join(unit_info))
 
 
 class UnitDefinition(SbmlObject):
-    def __init__(self, udid, name=None):
+    def __init__(self, udid):
         self.id = udid
-        self.name = name
         self.listOfUnits = []
+
+    def __str__(self):
+        return ' %s=%s' % (self.id, '; '.join(
+            [str(unit) for unit in self.listOfUnits]))
 
     def add_unit(self, unit):
         self.listOfUnits.append(unit)
@@ -51,8 +58,98 @@ class UnitDefinition(SbmlObject):
 
 
 class Compartment(SbmlObject):
-    def __init__(self, compid, constant, name=None, size=None):
+    def __init__(self, compid, name=None, size=None, constant=None):
         self.id = compid
         self.size = size
         self.constant = constant
         self.name = name
+
+    def __str__(self):
+        result = ' ' + self.id
+        if self.size:
+            result = '%s=%s' % (result, self.size)
+        if self.name:
+            result = '%s "%s"' % (result, self.name)
+        return result
+
+
+class Species(SbmlObject):
+    def __init__(self, sid, compartment, initial_amount,
+                 has_only_substance_units=False, boundary_condition=False,
+                 constant=False, name=None):
+        self.id = sid
+        self.compartment = compartment
+        self.initialAmount = initial_amount
+        self.hasOnlySubstanceUnits = has_only_substance_units
+        self.boundaryCondition = boundary_condition
+        self.constant = constant
+        self.name = name
+
+    def __str__(self):
+        sid = '[%s]' % self.id if self.constant else self.id
+        bool_attr = 's' if self.hasOnlySubstanceUnits else ''
+        bool_attr = bool_attr + 'b' if self.boundaryCondition else bool_attr
+        bool_attr = bool_attr + 'c' if self.constant else bool_attr
+        result = ' %s:%s=%s %s' % (
+        self.compartment, sid, self.initialAmount, bool_attr)
+        result = '%s "%s"' % (
+        result, self.name) if self.name is not None else result
+        return result
+
+
+class Parameter(SbmlObject):
+    def __init__(self, pid, value, name=None, constant=False):
+        self.id = pid
+        self.value = value
+        self.name = name
+        self.constant = constant
+
+    def __str__(self):
+        result = ' %s=%s' % (self.id, self.value)
+        result = '%sv' % result if self.constant else result
+        result = '%s "%s"' % (
+        result, self.name) if self.name is not None else result
+        return result
+
+
+class Rule(SbmlObject):
+    def __init__(self, body, ruletype):
+        self.body = body
+        self.type = ruletype
+
+    def __str__(self):
+        if self.type == "rate":
+            return ' @rate:%s' % self.body
+        else:
+            return ' %s' % self.body
+
+
+class Event(SbmlObject):
+    def __init__(self, body):
+        self.body = body
+
+    def __str__(self):
+        return ' %s' % self.body
+
+
+class Reaction(SbmlObject):
+    def __init__(self, reacttype, rid, reagents, products, body, params=[],
+                 name=None):
+        self.type = reacttype
+        self.reagents = reagents
+        self.products = products
+        self.params = params
+        self.id = rid
+        self.name = name
+        self.body = body
+
+    def __str__(self):
+        reag = '+'.join([r.id for r in self.reagents])
+        prod = '+'.join([p.id for p in self.products])
+        param = ','.join(
+            ['%s=%s' % (param.id, param.value) for param in self.params])
+        head = '@%s=%s "%s"' % (self.type, self.id,
+                                self.name) if self.name is not None else '@%s=%s' % (
+        self.type, self.id)
+        result = '%s\n %s -> %s\n %s' % (head, reag, prod, self.body)
+        return '%s : %s' % (result, param) if len(param) > 0 else result
--- SBMLsite/tests/test_sbml_classes.py
+++ SBMLsite/tests/test_sbml_classes.py
@@ -11,8 +11,16 @@ class TestSbml:
         self.my_comp2 = Compartment("id2")
         self.my_spe = Species("id", "cell", 0)
         self.my_spe2 = Species("id", "cell", 0, True, True, True, "test")
+        self.my_spe3 = Species("id2", "cell", 0, True, True, True, "test2")
         self.my_param = Parameter("k1", 10)
         self.my_param2 = Parameter("k2", 10, "test", True)
+        self.my_rule = Rule("v = 1 + 0.5*t", "assignment")
+        self.my_rule2 = Rule("v2=1", "rate")
+        self.react = Reaction("r", "Binding", [self.my_spe, self.my_spe3],
+                              [self.my_spe3, self.my_spe], "k2*Gene")
+        self.react2 = Reaction("rr", "Binding", [self.my_spe3, self.my_spe],
+                               [self.my_spe, self.my_spe3], "k2*Gene",
+                               [self.my_param, self.my_param2], "TEST")
 
     def teardown(self):
         pass
@@ -65,3 +73,17 @@ class TestParameter(TestSbml):
     def test_str(self):
         assert_equals(str(self.my_param), ' k1=10')
         assert_equals(str(self.my_param2), ' k2=10v "test"')
+
+
+class TestRule(TestSbml):
+    def test_str(self):
+        assert_equals(str(self.my_rule), ' v = 1 + 0.5*t')
+        assert_equals(str(self.my_rule2), ' @rate:v2=1')
+
+
+class TestReaction(TestSbml):
+    def test_str(self):
+        assert_equals(str(self.react),
+                      '@r=Binding\n id+id2 -> id2+id\n k2*Gene')
+        assert_equals(str(self.react2),
+                      '@rr=Binding "TEST"\n id2+id -> id+id2\n k2*Gene : k1=10,k2=10')
