4ac4558ca3e8cf74126903b9d12977d7e2db216c
Alexander Anatoljewitsch Steinke <a.steinke2@ncl.ac.uk>
2017-03-03 16:32:13 +0000

    Session Middlware and PickleSerializer

--- SBMLsite/urls.py
+++ SBMLsite/urls.py
@@ -18,4 +18,5 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^test', views.test, name='test'),
 ]
--- SBMLsite/views.py
+++ SBMLsite/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from SBMLshort.sbml import *
 
 def index(request):
     template = loader.get_template('index.html')
+    print(request.session['test'].__dict__)
+    print(request.session['test'].to_json())
+    context = {
+        'tests': "TEST",
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def test(request):
+    template = loader.get_template('index.html')
+    unit = Unit('test', -1, 0, 1)
+    ud = UnitDefinition("id")
+    ud.add_unit(unit)
+    request.session['test'] = ud
     context = {
         'tests': "TEST",
     }
